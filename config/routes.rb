08eca995f408ee156ab429b39a0c3a43ad113d28
yodev <yodev2177@gmail.com>
2020-06-02 13:07:07 +0900

    Fix typo  resources修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  # エラー原因: resourcesに対して単数となっていた
+  # 修正の意図: Fix type (blog => blogs)
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
