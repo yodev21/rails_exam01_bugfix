b4575b5ab4dfc50d12f754fd923a0056caf4bdc5
yodev <yodev2177@gmail.com>
2020-06-02 13:10:49 +0900

    Fix typo blogモデルのhas_manyを複数形に修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  has_many :comment
+  # エラー原因: has_manyに対して単数系出会った
+  # 修正の意図: Fix type (comments => comment)
+  has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true
